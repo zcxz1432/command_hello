21d32686f08920b99c6fcaf9d5b574d31ffa36cf
zcxz1432 <56594496+zcxz1432@users.noreply.github.com>
2020-04-18 18:28:51 +0900

    GitHub for Git pull

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 // For command line git tutorial
 // Local repository modification 
+// GitHub modification
 print("Hello World")
 print("Tell your World")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
 
